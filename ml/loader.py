96bb34a867599059c832b5a7c8235bacc25b49a4
Dominika <domido56@gmailcom>
2026-05-06 23:28:31 +0200

    przetwarzanie danych z integracja sql i ponowne szkolenie modeli

--- ml/loader.py
+++ ml/loader.py
@@ -2,19 +2,25 @@ import joblib
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sqlalchemy import create_engine
 
-features = ['volatile acidity', 'total sulfur dioxide', 'chlorides', 'sulphates']
-target = 'color_bin'
+
+features = ['VolatileAcidity', 'TotalSulfurDioxide', 'Chlorides', 'Sulphates']
+target = 'ColorBin'
 
 def get_full_dataset():
-    red = pd.read_csv('data/winequality-red.csv', sep=';')
-    white = pd.read_csv('data/winequality-white.csv', sep=';')
+    engine = create_engine("mssql+pyodbc://./WineWarehouse?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes")
 
-    red['color_bin'] = 1
-    white['color_bin'] = 0
+    query = """
+    SELECT
+        VolatileAcidity,
+        TotalSulfurDioxide,
+        Chlorides,
+        Sulphates,
+        ColorBin
+    FROM WineData"""
 
-    # polaczenie zbiorow
-    df = pd.concat([red, white], ignore_index=True)
+    df = pd.read_sql(query, engine)
     return df
 
 def get_test_data():
@@ -31,3 +37,14 @@ def load_models():
     svm = joblib.load("models/svm.pkl")
     return rf, knn, svm
 
+if __name__ == "__main__":
+    try:
+        print("test")
+        X_test, y_test = get_test_data()
+        print(f"pobrano {len(X_test)} rekordow testowych")
+        print("\npierwsze 5 wierszy danych:")
+        print(X_test.head())
+        print("\npierwsze 5 etykiet:")
+        print(y_test.head())
+    except Exception as e:
+        print(f"bład testu: {e}")
